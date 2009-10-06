95f5ddbf2309fb5a3660512722106fc6f386d09f
Sean Huber <shuber@huberry.com>
2009-10-06 15:37:20 -0700

    revert to attr_accessors and reload memoized methods when calling to_html

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -4,8 +4,7 @@ class Calendar
   
   undef_method :id
   
-  attr_reader :events
-  attr_accessor :options
+  attr_accessor :events, :month, :options, :year
   
   def self.default_options
     @default_options ||= {
@@ -19,12 +18,12 @@ class Calendar
   end
   
   def initialize(year = Time.now.year, month = Time.now.month, events = [], options = {})
-    @year, @month, @events, @options = year, month, events, self.class.default_options.merge(options)
+    self.year, self.month, self.events, self.options = year, month, events, self.class.default_options.merge(options)
     yield self if block_given?
   end
   
   def date
-    Date.civil(@year, @month, 1)
+    Date.civil(self.year, self.month, 1)
   end
   memoize :date
   
@@ -38,11 +37,13 @@ class Calendar
   end
   
   def to_html
+    date(:reload)
+    weeks(:reload)
     render_with_markaby
   end
   
   def weeks
-    days_in_month = Time.days_in_month(@month, @year)
+    days_in_month = Time.days_in_month(self.month, self.year)
     starting_day = date.beginning_of_week() -1.day + beginning_of_week.days
     ending_day = (date + days_in_month).end_of_week() -1.day + beginning_of_week.days
     (starting_day..ending_day).to_a.in_groups_of(7)
