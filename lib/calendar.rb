4f1a742d8bd660199e5ec37f42652e187daa341f
Sean Huber <shuber@huberry.com>
2009-10-06 12:03:48 -0700

    class and id the markup

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -6,12 +6,12 @@ class Calendar
   
   def self.default_options
     @default_options ||= {
-      :id => 'calendar',
-      :beginning_of_week => 0,
-      :event_id => :id,
-      :event_title => :title,
-      :event_start => :starts_at,
-      :event_end => :ends_at
+      :id                 => 'calendar',
+      :beginning_of_week  => 0,
+      :event_id           => :id,
+      :event_title        => :title,
+      :event_start        => :starts_at,
+      :event_end          => :ends_at
     }
   end
   
@@ -27,6 +27,7 @@ class Calendar
     weeks = generate_weeks_for(date)
     render date, weeks
   end
+  alias_method :to_html, :generate
   
   def method_missing(method, *args)
     method.to_s =~ /^([^=]+)(=?)$/
@@ -46,41 +47,67 @@ class Calendar
     def render(date, weeks)
       calendar = self
       Markaby::Builder.new do
-        div.calendar.send("#{calendar.id}!") do
+        div.calendar.send(date.strftime('%B').downcase).send("#{calendar.id}!") do
           div.header do
             table do
-              tr.navigation do
-                td.previous_month date.last_month.strftime('%B'), :colspan => 2
-                td.current_month date.strftime('%B %Y'), :colspan => 3
-                td.next_month date.next_month.strftime('%B'), :colspan => 2
-              end
-              tr.labels do
-                weeks.first.each do |day|
-                  td.day.label day.strftime('%a')
+              tbody do
+                tr.navigation do
+                  td.previous_month date.last_month.strftime('%B'), :colspan => 2
+                  td.current_month date.strftime('%B %Y'), :colspan => 3
+                  td.next_month date.next_month.strftime('%B'), :colspan => 2
+                end
+                tr.labels do
+                  weeks.first.each do |day|
+                    day_label = td.day
+                    day_label = day_label.send(day.strftime('%A').downcase)
+                    day_label = day_label.today if day.cwday == Time.now.to_date.cwday
+                    day_label.label day.strftime('%a')
+                  end
                 end
               end
             end
           end
           div.body do
             weeks.each do |week|
-              div.week do
-                table do
-                  tr.labels do
-                    week.each do |day|
-                      td.day.label day.strftime('%d').gsub(/^0/, '') # add current class and today
+              div.week.send("#{calendar.id}_week_#{week.first}_#{week.last}!") do
+                table.send("#{calendar.id}_labels_#{week.first}_#{week.last}!") do
+                  tbody do
+                    tr.labels do
+                      week.each do |day|
+                        day_label = td.day
+                        day_label = day_label.send(day.month == date.month ? 'current_month' : day < date ? 'previous_month' : 'next_month')
+                        day_label = day_label.send([6, 7].include?(day.cwday) ? 'weekend' : 'weekday')
+                        day_label = day_label.send(day.strftime('%A').downcase)
+                        day_label = day_label.send(day.strftime('%B').downcase)
+                        day_label = day_label.send("day_#{day.strftime('%d').gsub(/^0/, '')}")
+                        day_label = day_label.today if day == Time.now.to_date
+                        day_label.label day.strftime('%d').gsub(/^0/, '')
+                      end
                     end
                   end
                 end
-                div.days do
-                  table.grid do
-                    tr do
-                      week.each do |day| 
-                        td.day.send("day_#{day}!") {}
+                div.days.send("#{calendar.id}_days_#{week.first}_#{week.last}!") do
+                  table.grid.send("#{calendar.id}_grid_#{week.first}_#{week.last}!") do
+                    tbody do
+                      tr do
+                        week.each do |day| 
+                          day_grid = td.day
+                          day_grid = day_grid.send(day.month == date.month ? 'current_month' : day < date ? 'previous_month' : 'next_month')
+                          day_grid = day_grid.send([6, 7].include?(day.cwday) ? 'weekend' : 'weekday')
+                          day_grid = day_grid.send(day.strftime('%A').downcase)
+                          day_grid = day_grid.send(day.strftime('%B').downcase)
+                          day_grid = day_grid.send("day_#{day.strftime('%d').gsub(/^0/, '')}")
+                          day_grid = day_grid.today if day == Time.now.to_date
+                          day_grid.send("#{calendar.id}_day_#{day}!") {}
+                        end
                       end
                     end
                   end
                   unless calendar.events.empty?
-                    table.events do
+                    table.events.send("#{calendar.id}_events_#{week.first}_#{week.last}!") do
+                      tbody do
+                        # TODO
+                      end
                     end
                   end
                 end
