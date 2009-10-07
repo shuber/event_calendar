95dca76b21b8b9ef0561a2b192cc31fc2a56b178
Sean Huber <shuber@huberry.com>
2009-10-06 20:08:15 -0700

    update event parsing logic

--- lib/calendar/week.rb
+++ lib/calendar/week.rb
@@ -10,14 +10,12 @@ class Calendar
     end
     
     def events
-      # week_events = Array.new(7)
-      # @events.each do |event|
-      #   weekday_index = event.send(@options[:event_start]).to_date.SOMETHING
-      #   week_events[weekday_index] << event
-      # end
-      
-      # [[], [], [], [], [], [], []]
-      
+      week_events = inject([]) do |week, day|
+        week << @events.select do |event| 
+          event_start, event_end = dates(event)
+          (event_start == day || (event_start < day && day == first)) && event_end >= day
+        end.map { |event| [event, days(event)] }
+      end
       # [
       #   [[event, 1], [], [event, 1], [event, 4]],
       #   [[event, 2], [], [], [], [], [event, 1]]
@@ -28,9 +26,12 @@ class Calendar
     
     protected
     
+      def dates(event)
+        [event.send(@options[:event_start]).to_date, event.send(@options[:event_end]).to_date]
+      end
+      
       def days(event)
-        event_start = event.send(@options[:event_start]).to_date
-        event_end = event.send(@options[:event_end]).to_date
+        event_start, event_end = dates(event)
         (event_end > last ? last : event_end) - (event_start < first ? first : event_start) + 1
       end
       
