6f6ab1012531a5e685e38c62e2d0eeba082c0719
Sean Huber <shuber@huberry.com>
2009-10-08 12:59:11 -0700

    update css

--- test/calendar_test.rb
+++ test/calendar_test.rb
@@ -15,6 +15,9 @@ class CalendarTest < Test::Unit::TestCase
     Timecop.freeze(Date.civil(2009, 10, 6)) do
       events = [
         Event.new('Event 1', Time.now, 10.minutes.from_now),
+        Event.new('Event 1a', Time.now, 10.minutes.from_now),
+        Event.new('Event 1b', Time.now, 10.minutes.from_now),
+        Event.new('Event 1c', Time.now, 10.minutes.from_now),
         Event.new('Event 2', 1.day.from_now, 1.5.days.from_now),
         Event.new('Event 3', Time.now, 3.days.from_now),
         Event.new('Event 4 has a longer title', 3.days.from_now, 5.days.from_now),
