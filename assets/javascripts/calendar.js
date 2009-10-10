27d91b3a98677ee7c243a7c24c4d3ef100bcda52
Sean Huber <shuber@huberry.com>
2009-10-09 18:22:42 -0700

    refactor js

--- assets/javascripts/calendar.js
+++ assets/javascripts/calendar.js
@@ -11,39 +11,36 @@ var Calendar = Class.create({
     this.options = this.options.merge(options || {});
     this.events = $$('#' + this.element.id + ' ' + this.options.get('events_css_path'));
     
-    this.add_fields_to_events();
-    this.add_hover_behavior_to_events();
+    this.events.each(function(event) {
+      this.add_fields_to_event(event);
+      this.add_hover_behavior_to_event(event);
+    }.bind(this));
     
     Calendar.instances.push(this);
   },
   
-  add_fields_to_events: function() {
-    this.events.each(function(event) {
-      event.fields = $$('#' + event.id + ' ' + this.options.get('event_fields_css_path')).inject({}, function(fields, element) {
-        fields[element.title] = element.innerHTML;
-        return fields;
-      });
-    }.bind(this));
+  add_fields_to_event: function(event) {
+    event.fields = $$('#' + event.id + ' ' + this.options.get('event_fields_css_path')).inject({}, function(fields, element) {
+      fields[element.title] = element.innerHTML;
+      return fields;
+    });
   },
   
-  add_hover_behavior_to_events: function() {
-    var hover_class_name = this.options.get('event_hover_class');
+  add_hover_behavior_to_event: function(event) {
+    var hover_class_name = this.options.get('event_hover_class');    
+    var related_events = this.related_events_for(event);
     
-    this.events.each(function(event) {
-      var related_events = this.related_events_for(event);
-      
-      event.observe('mouseover', function() {
-        related_events.each(function(related_event) {
-          related_event.addClassName(hover_class_name);
-        });
+    event.observe('mouseover', function() {
+      related_events.each(function(related_event) {
+        related_event.addClassName(hover_class_name);
       });
-      
-      event.observe('mouseout', function() {
-        related_events.each(function(related_event) {
-          related_event.removeClassName(hover_class_name);
-        });
+    });
+    
+    event.observe('mouseout', function() {
+      related_events.each(function(related_event) {
+        related_event.removeClassName(hover_class_name);
       });
-    }.bind(this));
+    });
   },
   
   related_events_for: function(event) {
