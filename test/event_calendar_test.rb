89067318b55b21d538ccd86f709da35e62e27c48
Sean Huber <shuber@huberry.com>
2010-01-21 14:10:32 -0800

    rename calendar_test to event_calendar_test

--- test/event_calendar_test.rb
+++ test/event_calendar_test.rb
@@ -4,7 +4,7 @@ Event = Struct.new(:title, :starts_at, :ends_at) do
   def id; object_id; end
 end
 
-class CalendarTest < Test::Unit::TestCase
+class EventCalendarTest < Test::Unit::TestCase
 
   should 'test this gem' do
     flunk
